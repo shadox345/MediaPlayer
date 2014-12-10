75af7dd0cb5a7cbedb4617b1895f24e348a5ddcc
Jakob Schulz <jakob.s5672@gmail.com>
2014-12-10 20:17:38 +0100

    added time formatter

--- src/TaggedFile.java
+++ src/TaggedFile.java
@@ -1,5 +1,9 @@
 import studiplayer.basic.BasicPlayer;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 /**
  * Created by Jakob on 08.12.2014.
  */
@@ -31,5 +35,17 @@ public class TaggedFile extends AudioFile {
 
     public String getFormattedPosition() {
         return "";
+    }
+
+    public static String timeFormatter(long microtime) {
+        if (microtime < 0) {
+            throw new RuntimeException("Negative time value provided");
+        } else if (microtime >= (100 * 60 * 1000 * 1000)) { // more than 100 minutes
+            throw new RuntimeException("Time value exceeds allowed format");
+        }
+
+        DateFormat formatter = new SimpleDateFormat("mm:ss");
+        String time = formatter.format(microtime/1000); // convert to sec*10^-3
+        return time;
     }
 }
